13f01a729cd99b055dfdfc0030cc788911c8be73
Renan Souza <renantrsouza@gmail.com>
2022-05-04 14:06:39 -0400

    Render buttons

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -34,7 +34,36 @@ export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            Hello World
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+                
+                <CloseButton />
+            </header>
+            
+            {!feedbackType ? (
+                <div className="flex py-8 gap-2 w-full">
+                    { Object.entries(feedBackTypes).map(([index, value])=>{
+                        return (
+                            <button
+                                key={index}
+                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                                onClick={() => setFeedbackType(index as FeedbackType)} //Err Argument of type string is not assignable to parameter of type
+                                type="button"
+                            >
+                                <img src={value.image.source} alt={value.image.alt} />
+                                
+                                <span>{value.title}</span>
+                            </button>
+                        )
+                    }) }
+                </div>
+            ):(
+                <p>Hello World</p>
+            ) }
+
+            <footer className="text-xs text-neutral-400">
+                Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br" target="_blank">Rocketseat</a>
+            </footer>
         </div>
     )
 }
