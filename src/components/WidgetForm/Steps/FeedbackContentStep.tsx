37c2a4dd86613131cc4ed61c076b0e87274311f6
Renan Souza <renantrsouza@gmail.com>
2022-05-04 16:00:49 -0400

    Add header, types and back function

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,39 @@
-export function FeedbackContentStep() {
-    return (
-        <>
-        </>
-    )
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedBackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackContentStepProp {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+}
+export function FeedbackContentStep({
+  feedbackType,
+  onFeedbackRestartRequested,
+}: FeedbackContentStepProp) {
+  const feedBackTypeInfo = feedBackTypes[feedbackType];
+  return (
+    <>
+      <header>
+        <button
+          type="button"
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img
+            src={feedBackTypeInfo.image.source}
+            alt={feedBackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
+          {feedBackTypeInfo.title}
+        </span>
+
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full"></div>
+    </>
+  );
 }
