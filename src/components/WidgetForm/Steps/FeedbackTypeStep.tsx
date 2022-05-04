3b9a18df8faae4ed1aba6f2781088e403abc058a
Renan Souza <renantrsouza@gmail.com>
2022-05-04 16:00:05 -0400

    Add types and imports

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,6 +1,36 @@
-export function FeedbackTypeStep() {
-    return (
-        <>
-        </>
-    )
+import { FeedbackType, feedBackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackTypeSetpProps {
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
+}
+export function FeedbackTypeStep({
+  onFeedbackTypeChanged,
+}: FeedbackTypeSetpProps) {
+  return (
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedBackTypes).map(([index, value]) => {
+          return (
+            <button
+              key={index}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              onClick={() => onFeedbackTypeChanged(index as FeedbackType)} //Err Argument of type string is not assignable to parameter of type
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
 }
