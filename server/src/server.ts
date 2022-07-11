9c91544a3ecb2493cfa614b67eda4b9243be7f6b
Renan Souza <renantrsouza@gmail.com>
2022-07-11 16:30:27 -0400

    add: send email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express'
 import { prisma } from './prisma'
+import nodemailer from 'nodemailer'
 
 const app = express()
 
 app.use(express.json())
+
+//MAiltrap
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "f4f02be34d91cb",
+      pass: "710e57f9849d77"
+    }
+});
 
 app.post('/feedbacks', async (req, res) => {
     const {type, comment, screenshot} = req.body
@@ -15,6 +26,20 @@ app.post('/feedbacks', async (req, res) => {
             screenshot: screenshot
         }
     })
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Renan Souza <renan.souza@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    })
+
     return res.status(201).json({data: feedback})
 })
+
 app.listen(3333, () => console.log('Sever is running on port 3333'))
