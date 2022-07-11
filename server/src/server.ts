67c9236aa33d4716e368f920ef7d04be1478ed3a
Renan Souza <renantrsouza@gmail.com>
2022-07-11 13:41:10 -0400

    create: route for submit feedback and save in database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,20 @@
 import express from 'express'
+import { prisma } from './prisma'
 
 const app = express()
 
+app.use(express.json())
+
+app.post('/feedbacks', async (req, res) => {
+    const {type, comment, screenshot} = req.body
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type: type,
+            comment: comment,
+            screenshot: screenshot
+        }
+    })
+    return res.status(201).json({data: feedback})
+})
 app.listen(3333, () => console.log('Sever is running on port 3333'))
